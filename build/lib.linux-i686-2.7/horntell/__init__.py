df659d2a111ba9774bb773ecf29b5ded9c1cf380
Sahil <sahilsarpal@gmail.com>
2015-03-09 11:04:10 +0530

    files removed from cached

--- build/lib.linux-i686-2.7/horntell/__init__.py
+++
@@ -1,7 +0,0 @@
-
-api_key = None
-api_base = 'https://api.horntell.com'
-
-# Resource
-from horntell.resource import (  # noqa
-    Charge)
--- build/lib.linux-i686-2.7/horntell/resource.py
+++
@@ -1,7 +0,0 @@
-import urllib
-import warnings
-import sys
-
-class Charge():
-    def create(self):
-        return '5'
