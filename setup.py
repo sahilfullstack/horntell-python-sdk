b009e487d6bc372ebfa79e84d58e1a34ce3f66aa
Mohit Mamoria <mohit.mamoria@gmail.com>
2015-03-31 07:32:26 +0000

    converted to tabs

--- setup.py
+++ setup.py
@@ -3,20 +3,20 @@ import sys
 import warnings
 
 try:
-    from setuptools import setup
+	from setuptools import setup
 except ImportError:
-    from distutils.core import setup
+	from distutils.core import setup
 
 try:
-    from distutils.command.build_py import build_py_2to3 as build_py
+	from distutils.command.build_py import build_py_2to3 as build_py
 except ImportError:
-    from distutils.command.build_py import build_py
+	from distutils.command.build_py import build_py
 
 setup(
-    name = 'horntell',
-    description = 'horntell python bindings',
+	name = 'horntell',
+	description = 'Python SDK for Horntell',
 
-    author = 'horntell',
-    author_email = 'mohit@horntell.com',
-    packages = ['horntell']
+	author = 'horntell',
+	author_email = 'mohit@horntell.com',
+	packages = ['horntell']
 )
