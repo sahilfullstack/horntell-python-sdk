090f798f853a35a769d8a0d6ffd0d2c887fa0ebb
Sahil <sahilsarpal@gmail.com>
2016-04-21 17:20:52 +0530

    unused files added in gitignore

--- .cgiserver.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-import http
-from http.server import CGIHTTPRequestHandler, HTTPServer
-
-handler = CGIHTTPRequestHandler
-handler.cgi_directories = ['/cgi-bin', '/htbin']  # this is the default
-server = HTTPServer(('localhost', 8081), handler)
-server.serve_forever()
